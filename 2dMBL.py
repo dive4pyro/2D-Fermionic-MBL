d00fedf502f172a21fd2204c99f5faf22f54494d
dive4pyro <joeyli415@gmail.com>
2020-09-13 11:50:57 -0700

    Update 2dMBL.py

--- 2dMBL.py
+++ 2dMBL.py
@@ -1,3 +1,8 @@
+'''
+Main Code
+
+(optimize all unitaries at once)
+'''
 import numpy as np
 from numpy import pi,cos,floor,kron,zeros
 from numpy.random import rand
@@ -24,18 +29,16 @@ def h(m,n):
 
 '''function to generate the untaries in the quantum circuit ansatz.
 creates a random unitary with particle number conservation'''
-def generate_unitary():
-    a1 = torch.rand(4,4)# 1 particle sector
-    a2 = torch.rand(6,6)# 2 particle sector
-    a3 = torch.rand(4,4)# 3 particle sector
+def generate_unitary(A):
+    #A is a list of [4x4 matrix, 6x6 matrix, 4x4 matrix]
 
     #construct the block diagonal random unitary matrix
     u = torch.zeros([16,16])
     u[0,0] = 1.       # 0 particle sector
     u[15,15] = 1      # 4 particle sector
-    u[1:5,1:5] = torch_expm(a1-a1.T)    # 1 particle sector
-    u[5:11,5:11] = torch_expm(a2-a2.T)  # 2 particle sector
-    u[11:15,11:15] = torch_expm(a3-a3.T)# 3 particle sector
+    u[1:5,1:5] = torch_expm(A[0]-A[0].T)    # 1 particle sector
+    u[5:11,5:11] = torch_expm(A[1]-A[1].T)  # 2 particle sector
+    u[11:15,11:15] = torch_expm(A[2]-A[2].T)# 3 particle sector
 
     #transform to the usual basis (u will no longer be block diagonal)
     u = permuteBasis(u,permute_order(4))
@@ -47,16 +50,27 @@ def dagger(u):
 
 ####################################################################################
 '''main part of the code'''
-#initialize all the to-be-optimized variables i.e. the unitaries
+#initialize all the to-be-optimized variables
 #store them in 2D lists
-# u = top layer, v = bottom layer
+
+#actual variables: Au and Av
+#these are the underlying variables which must be passed to the optimizer later
+Au = []; Av = []
+for i in range(int(N/2)):
+    Au.append([])
+    Av.append([])
+    for j in range(int(N/2)):
+        Au[i].append([torch.rand(4,4,requires_grad=True),torch.rand(6,6,requires_grad=True),torch.rand(4,4,requires_grad=True)])
+        Av[i].append([torch.rand(4,4,requires_grad=True),torch.rand(6,6,requires_grad=True),torch.rand(4,4,requires_grad=True)])
+
+# unitaries: u = top layer, v = bottom layer
 u = []; v = []
 for i in range(int(N/2)):
     u.append([])
     v.append([])
     for j in range(int(N/2)):
-        u[i].append(generate_unitary())
-        v[i].append(generate_unitary())
+        u[i].append(generate_unitary(Au[i,j]))
+        v[i].append(generate_unitary(Av[i,j]))
 
 
 
